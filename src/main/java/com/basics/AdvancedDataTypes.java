9f70466a593aed7e60e8881894e92d541ed2cbd8
Harshit Sharma <sharma1612harshit@gmail.com>
2021-11-26 19:08:26 +0530

    advanced data types

---
+++ src/main/java/com/basics/AdvancedDataTypes.java
@@ -0,0 +1,10 @@
+package com.basics;
+
+import java.util.*;
+
+public class AdvancedDataTypes {
+    String StringVar = "Hello World!";
+    int[] IntArray = new int[2];
+    ArrayList ArrayListVar = new ArrayList();
+    Map MapVar = new HashMap();
+}
--- src/main/java/com/basics/Basics.java
+++ src/main/java/com/basics/Basics.java
@@ -16,5 +16,21 @@ class Basics {
         System.out.println("\tfloat: " + dataTypes.FloatingPoint);
         System.out.println("\tlong: " + dataTypes.Long);
         System.out.println("\tdouble: " + dataTypes.Double);
+
+        // initiate a new instance of class DataTypes present in same package
+        com.basics.AdvancedDataTypes advDataTypes = new com.basics.AdvancedDataTypes();
+        System.out.println("\nAdvanced Data Types -");
+        System.out.println("\tString: " + advDataTypes.StringVar);
+        // Add items to primitive type array
+        advDataTypes.IntArray[0] = 1;
+        advDataTypes.IntArray[1] = 2;
+        System.out.println("\tint[0]: " + advDataTypes.IntArray[0]);
+        // Add items to array list
+        advDataTypes.ArrayListVar.add(0, "item1");
+        advDataTypes.ArrayListVar.add(1, 2);
+        System.out.println("\tArrayList: " + advDataTypes.ArrayListVar);
+        // Add keys to map
+        advDataTypes.MapVar.put("Key1", "value1");
+        System.out.println("\tMap: " + advDataTypes.MapVar);
     }
 }
