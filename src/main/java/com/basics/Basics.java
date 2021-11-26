e5ebe194c076b6ae9510e1e2d3f65b2c36b6ddd7
Harshit Sharma <sharma1612harshit@gmail.com>
2021-11-26 15:53:34 +0530

    primitive data types

--- src/main/java/com/basics/Basics.java
+++ src/main/java/com/basics/Basics.java
@@ -2,6 +2,19 @@ package com.basics;
 
 class Basics {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Java basics!");
+
+        // initiate a new instance of class DataTypes present in same package
+        com.basics.PrimitiveDataTypes dataTypes = new com.basics.PrimitiveDataTypes();
+
+        System.out.println("\nPrimitive Data Types (in increasing order of size) -");
+        System.out.println("\tboolean: " + dataTypes.Boolean);
+        System.out.println("\tbyte: " + dataTypes.Byte);
+        System.out.println("\tshort: " + dataTypes.Short);
+        System.out.println("\tchar: " + dataTypes.Char);
+        System.out.println("\tint: " + dataTypes.Integer);
+        System.out.println("\tfloat: " + dataTypes.FloatingPoint);
+        System.out.println("\tlong: " + dataTypes.Long);
+        System.out.println("\tdouble: " + dataTypes.Double);
     }
 }
---
+++ src/main/java/com/basics/PrimitiveDataTypes.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+class PrimitiveDataTypes {
+    boolean Boolean = true; // 1 bit, true or false
+    byte Byte = 127; // 8 bit, -128 to 127
+    char Char = 'c';
+    short Short = 32767; // 16 bit, 32,768 to 32,767
+    int Integer = 2147483647; // 32 bit, -2147483648 to 2147483647
+    float FloatingPoint = 10/3; // 32 bit IEEE 754 floating point,
+    long Long = 123456789; // 64 bit , -2^63 to (2^63)-1
+    double Double = 10.0/3; // 64 bit IEEE 754 floating point,
+}
