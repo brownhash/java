4f1e61aee36b75045b348e2d17ec023932894d54
Harshit Sharma <sharma1612harshit@gmail.com>
2021-12-05 18:21:39 +0530

    add synchronous system

---
+++ src/main/java/com/advanced/vertx/basics/Synchronous.java
@@ -0,0 +1,61 @@
+package com.advanced.vertx.basics;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+This code is to show the anti of Vert.x pattern i.e. Synchronous
+So that we clearly understand the benefits of Asynchronous
+*/
+public class Synchronous {
+    public static void main(String[] args) throws Throwable {
+        ServerSocket server = new ServerSocket();
+        server.bind(new InetSocketAddress(3000));
+        while (true) {
+            Socket socket = server.accept();
+            new Thread(clientHandler(socket)).start();
+        }
+    }
+
+    /*
+    Listens reader fo keywords,
+    unless the keyword is not `/quit`,
+    takes the keyword and echoes it
+    */
+    private static Runnable clientHandler(Socket socket) {
+        return () -> {
+            try (
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()));
+                    PrintWriter writer = new PrintWriter(
+                            new OutputStreamWriter(socket.getOutputStream()))) {
+                String line = "";
+                while (!"/quit".equals(line)) {
+                    line = reader.readLine();
+                    System.out.println("~ " + line);
+                    writer.write(line + "\n");
+                    writer.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+    }
+}
+
+/*
+Compile the code,
+    $ javac ...Synchronous.java
+
+Run the server,
+    $ java ...Synchronous
+
+To interact,
+    $ netcat localhost 3000     # start a connection with server
+    $ hello world!              # input some string
+    > hello world!              # receive the same string as output
+    $ /quit                     # enter `/quit` keyword
+    > /quit                     # receive the same as output and exits
+*/
